a4d9d4c7b7af13629a4040bb7cfcca568d21e201
Diogo Alexandre <diogo.melo@compasso.com.br>
2022-04-26 13:34:48 -0300

    fix: import typeorm module correctly

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,22 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { TypeOrmConfig } from '../../shared/config/typeorm.config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  providers: [
-    {
-      provide: TypeOrmConfig,
-      useFactory: (configService: ConfigService) => new TypeOrmConfig(configService),
-      inject: [ConfigService]
-    },
-    {
-      provide: TypeOrmModule,
-      useFactory: (config: DataSource) => TypeOrmModule.forRoot(config.options),
-      inject: [TypeOrmConfig]
-    }
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(new TypeOrmConfig(new ConfigService()).options)
   ]
 })
 export class DatabaseModule { }
